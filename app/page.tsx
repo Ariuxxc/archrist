2854352061a6212ed614695b879fc989fd3b35bf
Ariuxxc <ariuxchristian@gmail.com>
2024-10-17 15:10:40 +0000

    cm

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@
 import Image from "next/image";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import pdp from './assets/pdp.png';
-import logoJS from './assets/LogoJS.png'
+import logoJS from './assets/LogoJS.png';
+import img from './assets/nextjs.png';
+import './responsive.css';
 export default function Home() {
   return (
     <div className="bg-dark text-white min-h-screen w-full flex" style={{height: "100%"}}>
@@ -14,7 +16,7 @@ export default function Home() {
       />
       <h3>Christian Ferreol</h3><br></br>
     
-    <div className="card bg-dark" style={{borderColor: "orange", color: "white", width: "50%", marginTop: "70px", height: "500px"}}>
+    <div className="card bg-dark" style={{borderColor: "orange", color: "white", width: "70%", marginTop: "70px", height: "600px"}}>
       <div className="card-body" >
         <h5 className="card-title">Christian Ferreol</h5>
         <p>Je me nomme  Jean-Christian , apprenant dans le developpement web fullstack, agé de 14 ans</p>
@@ -24,20 +26,18 @@ export default function Home() {
         <p>Pour être honnête, je ne suis pas encore compétent mais j'ai déjà réussi à créer des sites web fonctionnels, Mais malheuresement je n'ai pas pu les mettre en ligne pour des raisons personnelles</p>
         <p>Actullement, j'suis encore un élève certe mais j'ai pour objectif de devenir un expert dans le domaine</p>
         <p>J'espère que vous apprécierez mon travail</p>
-        <p>Merci pour votre temps</p>
-      </div><br></br><br></br><br></br>
-      
-     
-    </div>
-    <div className="card bg-dark" style={{borderColor: "orange", color: "white", width: "50%", marginTop: "70px", height: "500px"}}>
-      <div className="card-body">
+        <p>Merci pour votre temps</p><br></br>
+        <h3><u>Languages et FrameWorks utilisés</u></h3><br></br>
         <Image 
         src={logoJS}
         alt="logoJS"
-        style={{width: '110px', height: '100px', marginLeft: "50px"}}
+        style={{width: '110px', height: '100px'}}
         />
       </div>
+      
+     
     </div>
+    
     </div>
   );
 }
