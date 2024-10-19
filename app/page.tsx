20e46f4a93c6863e119ee1e38c4398b648b44747
Ariuxxc <ariuxchristian@gmail.com>
2024-10-19 11:20:50 +0000

    test

--- app/page.tsx
+++ app/page.tsx
@@ -21,11 +21,11 @@ export default function Home() {
         <h5 className="card-title">Christian Ferreol</h5>
         <p>Je me nomme  Jean-Christian , apprenant dans le developpement web fullstack, agé de 14 ans</p>
         <p>Depuis mes 13 ans, j'ai découvert ma passion pour la programmation</p>
-        <p>J'ai déjà travaillé sur plusieurs projets en html, css, js, react, node, express et mongodb</p>
-        <p>J'ai déjà travaillé avec des frameworks comme bootstrap et tailwindcss</p>
-        <p>Pour être honnête, je ne suis pas encore compétent mais j'ai déjà réussi à créer des sites web fonctionnels, Mais malheuresement je n'ai pas pu les mettre en ligne pour des raisons personnelles</p>
-        <p>Actullement, j'suis encore un élève certe mais j'ai pour objectif de devenir un expert dans le domaine</p>
-        <p>J'espère que vous apprécierez mon travail</p>
+        <p>J ai déjà travaillé sur plusieurs projets en html, css, js, react, node, express et mongodb</p>
+        <p>J ai déjà travaillé avec des frameworks comme bootstrap et tailwindcss</p>
+        <p>Pour être honnête, je ne suis pas encore compétent mais j ai déjà réussi à créer des sites web fonctionnels, Mais malheuresement je n'ai pas pu les mettre en ligne pour des raisons personnelles</p>
+        <p>Actullement, j suis encore un élève certe mais j ai pour objectif de devenir un expert dans le domaine</p>
+        <p>J espère que vous apprécierez mon travail</p>
         <p>Merci pour votre temps</p><br></br>
         <h3><u>Languages et FrameWorks utilisés</u></h3><br></br>
         <Image 
