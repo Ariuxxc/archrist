5f00d07b0dcb2467238e89a71f584ba71080fe93
Ariuxxc <ariuxchristian@gmail.com>
2024-10-30 10:48:58 +0000

    upadte error

--- app/blog/admin-panel/page.tsx
+++ app/blog/admin-panel/page.tsx
@@ -22,15 +22,15 @@ export default function AdminPanel() {
     <div className="d-flex">
         <Sidebar />
         <div className="flex-grow-1 p-4">
-            <h1 className="mb-4">Panneau d'administration</h1>
+            <h1 className="mb-4">Panneau d&apos;administration</h1>
             <div className="card shadow-lg">
                 <div className="card-body">
-                    <h2 className="card-title font-bold mb-3">À quoi sert ce panneau d'administrateur ?</h2>
+                    <h2 className="card-title font-bold mb-3">À quoi sert ce panneau d&apos;administrateur ?</h2>
                     <p className="mb-3">Ce panneau vous aide à mieux contrôler et suivre vos posts de près. Il offre plusieurs fonctionnalités pour une gestion efficace de votre blog :</p>
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item">La rubrique <strong>Stats</strong> vous permet de suivre les statistiques de votre blog.</li>
                         <li className="list-group-item">La section <strong>Posts</strong> vous offre un contrôle total sur vos publications.</li>
-                        <li className="list-group-item">D'autres outils sont disponibles pour optimiser la gestion de votre contenu.</li>
+                        <li className="list-group-item">D&apos;autres outils sont disponibles pour optimiser la gestion de votre contenu.</li>
                     </ul>
                 </div>
             </div>
