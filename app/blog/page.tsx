5b8b84f8a2b3e69f41e6d8cc16d4debaf761b893
Ariuxxc <ariuxchristian@gmail.com>
2024-10-31 15:11:56 +0000

    update

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -36,14 +36,13 @@ export default function Blog() {
     <meta property="og:title" content="Mes publications - Blog de Christian Ferreol" />
     <meta property="og:description" content="Découvrez les dernières publications de Christian Ferreol sur le développement web." />
     <meta property="og:url" content="https://jean-christ.vercel.app/blog" />
-    <img src="./image.png" alt="Image description" />
     <meta property="og:type" content="website" />
  </head>
             <nav className="navbar" style={{ backgroundColor: '#1e1e1e', padding: '10px 20px', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', alignItems: 'center' }}>
-                <h2 className="text-xl font-bold" style={{ color: 'blue', flex: '1 1 100%', textAlign: 'center' }}>Blog Menu</h2>
+                <h2 className="text-xl font-bold" style={{ color: 'orange', flex: '1 1 100%', textAlign: 'center' }}>Blog Menu</h2>
                 <ul style={{ listStyleType: 'none', display: 'flex', margin: 0, padding: 0, flexWrap: 'wrap', justifyContent: 'center', width: '100%' }}>
                     <li style={{ margin: '0 15px' }}>
-                        <Link href="/blog">
+                        <Link href="/">
                         <button className="btn btn-outline-primary">Accueil</button></Link>
                     </li>
                     <li style={{ margin: '0 15px' }}>
@@ -70,7 +69,7 @@ export default function Blog() {
                 ))}
             </div>
               
-        
+         
             </>
         
     )
